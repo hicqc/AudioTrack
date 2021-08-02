5eed5aeb084e061e737098cc09babc6ee8d0e1be
曹庆超 <caoqingchao1@xiaomi.com>
2021-08-02 10:52:12 +0800

    add play

--- app/src/main/java/com/cqc/audiotrack/MainActivity.java
+++ app/src/main/java/com/cqc/audiotrack/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cqc.audiotrack;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.AudioTrack;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.cqc.audiotrack.controller.AudioTrackController;
 
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button button_play;
@@ -21,16 +25,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button_play = findViewById(R.id.button_play);
 
         button_play.setOnClickListener(this);
-        initAudioTrackPlayer();
+        initAudioTrackPlayer(this);
     }
 
-    private void initAudioTrackPlayer() {
 
+    private void initAudioTrackPlayer(Context context) {
+        audioTrackController = new AudioTrackController(context);
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_play:
+                audioTrackController.play();
+                break;
+            default:
+                break;
+        }
 
     }
 }
--- app/src/main/java/com/cqc/audiotrack/controller/AudioTrackController.java
+++ app/src/main/java/com/cqc/audiotrack/controller/AudioTrackController.java
@@ -1,5 +1,6 @@
 package com.cqc.audiotrack.controller;
 
+import android.content.Context;
 import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioTrack;
@@ -7,9 +8,22 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
+import com.cqc.audiotrack.R;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class AudioTrackController {
     AudioTrack audioTrack;
+    Context context;
+    BufferedInputStream bufferedInputStream;
+
+    public AudioTrackController(Context context) {
+        initAudioTrack();
+        this.context = context;
+    }
 
     public void initAudioTrack() {
         int minBufferSize = AudioTrack.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_STEREO,
@@ -25,17 +39,32 @@ public class AudioTrackController {
          */
         audioTrack = new AudioTrack.Builder()
                 .setAudioAttributes(new AudioAttributes.Builder()   //用于封装描述有关音频流的信息的属性集合的类.
-                        .setUsage(AudioAttributes.USAGE_ALARM)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .setUsage(AudioAttributes.USAGE_MEDIA)  //set声音用途
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC) //set播放内容的类型
                         .build())
                 .setAudioFormat(new AudioFormat.Builder()   //用于访问多个音频格式和信道配置常量。
-                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-                        .setSampleRate(44100)
-                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)
+                        .setEncoding(AudioFormat.ENCODING_PCM_16BIT)    //采样位深
+                        .setSampleRate(48000)   //采样率
+                        .setChannelMask(AudioFormat.CHANNEL_OUT_STEREO)     //信道掩码
                         .build())
+                .setTransferMode(AudioTrack.MODE_STREAM)
                 .setBufferSizeInBytes(minBufferSize)
                 .build();
     }
 
 
+    public void play() {
+        loadFile();
+        new Runnable() {
+            @Override
+            public void run() {
+                //TODO
+            }
+        };
+    }
+
+    private void loadFile() {
+        InputStream inputStream = context.getResources().openRawResource(R.raw.music);
+        bufferedInputStream = new BufferedInputStream(inputStream);
+    }
 }
